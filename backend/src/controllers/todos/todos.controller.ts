5edb71e025e55b684f065729fe69277982c69dd2
gflores05 <gabriel.flores0503@gmail.com>
2021-03-04 01:45:22 -0600

    feat(todos): todo controller

--- backend/src/controllers/todos/todos.controller.ts
+++ backend/src/controllers/todos/todos.controller.ts
@@ -1,11 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TodoService } from 'src/services/todos.service';
 import { Todo } from '../../models';
 
 @Controller('todos')
 export class TodosController {
 
+  constructor(private service: TodoService) {}
+
   @Get()
-  findAll(): Todo[] {
-    return [];
+  async findAll(): Promise<Todo[]> {
+    return await this.service.findAll();
+  }
+
+  @Post()
+  async create(@Body() obj: Todo) {
+    await this.service.insert(obj);
+
+    return {status: "ok"}
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return await this.service.findById(id);
+  }
+
+  @Put()
+  async update(@Body() obj: Todo) {
+    await this.service.update(obj);
+    return {status: "ok"}
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    await this.service.delete(id);
+    return {status: "ok"}
   }
 }
